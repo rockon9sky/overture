c8ec480cf716be70bc3cae2c4cd7f2733901ab09
Max Lv <max.c.lv@gmail.com>
2017-11-19 10:51:13 +0800

    Fix includes for NDK r16

--- main/main.go
+++ main/main.go
@@ -6,12 +6,13 @@ package main
 
 /*
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <sys/uio.h>
-#include <unistd.h>
 
 #define ANCIL_FD_BUFFER(n) \
     struct { \
